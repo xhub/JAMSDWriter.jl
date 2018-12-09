a9c16e8683a53281fe9645883ae99511afecdeb1
Olivier Huber <oli.huber@gmail.com>
2018-12-09 01:26:01 +0100

    add Mac OS

--- deps/build.jl
+++ deps/build.jl
@@ -1,5 +1,3 @@
-using BinaryProvider
-
 using BinDeps
 
 @BinDeps.setup
@@ -9,21 +7,18 @@ libjamsd = library_dependency("libjamsd", aliases=["jamsd"])
 # Download binaries from hosted location
 bin_prefix = "https://nullptr.fr/lib"
 
-download_info = Dict(
-Linux(:x86_64, :glibc)  => ("$bin_prefix-x86_64-linux-gnu/libjamsd.so", "c073dda4e6282aa6ef6cf2f901a86b771fda07412cfc1be735d093446695c47e"),
-Windows(:i686)          => ("$bin_prefix-win32/jamsd.dll", "aba26dcb218273545f4aff706a80da90ae74ab32d6aec17ca91efafa5bcf3b29"),
-Windows(:x86_64)        => ("$bin_prefix-win64/jamsd.dll", "bee639c681701e2227b939ca47b8eac95b869bd4b0428f83910f7653de6c0e94"),
-#    MacOS(:x86_64)          => ("$bin_prefix/libfoo.x86_64-apple-darwin14.tar.gz", "fcc268772d6f21d65b45fcf3854a3142679b78e53c7673dac26c95d6ccc89a24"),
-)
-
 # TODO with latest Julia
 if VERSION < v"0.7"
-    iswin = is_windows()
+    iswin   = is_windows()
     islinux = is_linux()
+    ismac   = is_apple()
 else
-    iswin = Sys.iswindows()
+    iswin   = Sys.iswindows()
     islinux = Sys.islinux()
+    isapple = Sys.isapple()
 end
+
+# TODO remove all is(win|linux|apple) and just have a switch on sys.ARCH?
 
 if iswin
     if Sys.ARCH == :x86_64
@@ -36,6 +31,12 @@ end
 if islinux
     if Sys.ARCH == :x86_64
         provides(Binaries, URI("$bin_prefix-x86_64-linux-gnu/libjamsd.tar.xz"), libjamsd, os = :Linux)
+    end
+end
+
+if isapple
+    if Sys.ARCH == x86_64
+        provides(Binaries, URI("$bin_prefix-macosx/libjamsd.tar.xz"), libjamsd, os = :Darwin)
     end
 end
 
